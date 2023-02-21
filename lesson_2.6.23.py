8cb5c36db5b015a58c36ce1cf30e72ca58f88071
550tealeaves <nastasia.claire89@gmail.com>
2023-02-21 17:31:16 -0500

    added python file

---
+++ lesson_2.6.23.py
@@ -0,0 +1,18 @@
+1+1
+2**4
+True
+type(False)
+2**5
+type("hello world")
+type('hola mundo')
+type(1.5)
+type(65)
+type("arrow")
+type(["coffe", "yogurt", "oatmeal"])
+type([1,5,7,8])
+type([1, "bananas", 7, "eight"])
+my_variable = "hello"
+my_variable.upper() 
+'any string'.upper()
+'RANDOM STRING'.lower()
+'nastasia'.upper()
