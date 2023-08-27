7c7f7c40ed3d8b40b09e01a35a3ede530eb6b018
550tealeaves <nastasia.claire89@gmail.com>
2023-08-27 12:37:05 -0400

    added more lessons

--- Mimo_list-operators.py
+++ Mimo_list-operators.py
@@ -30,3 +30,61 @@ print(min(shares)) #3
 weekly_profits = [50, 60, 70, 20]
 max_profit = max(weekly_profits) #will store max value (70) inside var max_profit
 print(max_profit) #70
+
+#SORTING DATA
+#Sorted lists are useful when we want to understand where individual data points, like scores, stand in relations to others
+
+#To sort list like scores, code list_name.sort() - will change the list 
+scores = [3, 6, 1, 12]
+scores.sort() # will sort list in ascending order
+print(scores) # 1, 3, 6, 12
+
+#Will also sort float values and integers based on numeric values
+prices = [13, 9.99, 10, 7.5]
+prices.sort()
+print(prices) #7.5, 9.99, 10, 13
+
+
+temperatures = [-1, -3, 2]
+temperatures.sort()
+print(temperatures) #-3, -1, 2
+
+#.sort() will sort strings alphabetically
+names = ['Chloe', 'Ana', 'Bill']
+names.sort()
+print(names) #Ana, Bill, Chloe
+
+
+friend_requests = [3, 1, 2]
+friend_requests.sort()
+print(friend_requests) #1, 2, 3
+
+
+active_users = [103, 200, 140, 288]
+active_users.sort()
+print(active_users) #103, 140, 200, 288
+
+sold_tickets = [7, 30, 20, 12]
+print(sold_tickets) #7, 30, 20, 12
+sold_tickets.sort() # sorts the list numerically
+print(sold_tickets) #7, 12, 20, 30
+
+
+test_points = [97, 76, 81]
+test_points.sort()
+print(test_points) #76, 81, 97
+
+
+ingredients = [65.5, 21, 45]
+ingredients.sort()
+print(ingredients) #21, 45, 65.5
+
+
+overview = [3, -1, 4, -2]
+overview.sort()
+print(overview) #-2, -1, 3, 4
+
+
+destinations = ['Germany', 'Italy', 'Austria']
+destinations.sort()
+print(destinations) #'Austria', 'Germany', 'Italy'
