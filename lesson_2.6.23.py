af03242d073f5ee21b8a04b6e70ebdba790859b5
550tealeaves <nastasia.claire89@gmail.com>
2023-08-28 10:10:43 -0400

    adding more code

--- lesson_2.6.23.py
+++ lesson_2.6.23.py
@@ -24,3 +24,57 @@ classes
 for drama in "entourage":
     print(drama)
 
+
+
+#BOOLEANS
+#can generate True/False by setting variables = to a negated variable
+open_slot = True
+is_available = not open_slot
+print(is_available) #False
+
+is_even = not False
+print(is_even) #True
+
+
+flight_mode = True
+update = not flight_mode
+print(update) #False 
+
+
+flight_mode = True
+updating = not flight_mode #False
+use_apps = not updating #True
+print(use_apps) #True
+
+
+#Using != to check if variables are different
+num_1 = 17
+num_2 = 23
+print(num_1 != num_2) #True
+
+
+tries_left = 1
+game_over = tries_left == 0
+print("Game over:") #Game over:
+print(game_over) #False
+
+
+player_1 = 0
+player_2 = 10
+player_1 = 10 #reassigns new value to existing variable
+
+same_score = player_1 == player_2 #see if variables are equal to each other
+
+print("It's a tie:") #It's a tie
+print(same_score) #True
+
+
+#Can reassign a value to itself
+downloaded = 9
+downloaded = downloaded + 1
+print(downloaded) #10
+
+in_progress = downloaded != 10  #downloaded doesn't equal 10
+
+print("Download finished:")
+print(in_progress) #False b/c 10 is equal to 10
