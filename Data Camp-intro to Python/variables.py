d3baedf111f6bd71c9d77abbfbe2afeb49b272bf
550tealeaves <nastasia.claire89@gmail.com>
2024-05-27 13:07:07 -0400

    added more notes

--- Data Camp-intro to Python/variables.py
+++ Data Camp-intro to Python/variables.py
@@ -14,3 +14,23 @@ doubleintro = intro + intro
 
 # Print out doubleintro
 print(doubleintro) #Hello! How are you?Hello! How are you?
+
+
+# Definition of savings and total_savings
+savings = 100
+total_savings = 150
+
+
+# Fix the printout
+
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+print(pi_float)
+
+#Can use multiplication in strings to repeat certain worlds
+"I said " + ("Could you come here, please? " * 3) + "Melissa"
