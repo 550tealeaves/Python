9e4ebd62ea771a90d5533185782da0dbb8cd1da2
550tealeaves <nastasia.claire89@gmail.com>
2024-06-02 14:03:14 -0400

    adding notes on functions

---
+++ Data Camp-intro to Python/functions.py
@@ -0,0 +1,55 @@
+# ****FUNCTIONS****
+# - reusable code that performs specific task
+
+#Python has several built-in functions
+
+#MAX()
+#Ex: find max value in a list
+fam = [1.73, 1.68, 1.71, 1.89]
+max(fam) #1.89
+#function max works like a black box - passed it a list of values, then the implementation of max() that we don't know produces an output
+
+#Can assign result of a function call to a new variable
+tallest = max(fam)
+print(tallest) #1.89
+
+#ROUND()
+# takes 2 inputs (# for rounding, & precision (# decimals))
+round(1.68, 1) #1.7
+round(1.685545856, 6) #1.685546
+
+#Can use round w/o precision input - will round to nearest integer
+round(6.784324757574) #7
+
+
+#HELP()
+#Provides explanation on functions
+help(round) # round(number, ndigits=None)
+    # Round a number to a given precision in decimal digits.
+    # The return value is an integer if ndigits is omitted or None.  Otherwise
+    # the return value has the same type as the number.  ndigits may be negative.
+
+help(max) #max(...)
+#     max(iterable, *[, default=obj, key=func]) -> value
+#     max(arg1, arg2, *args, *[, key=func]) -> value
+#     With a single iterable argument, return its biggest item. The
+#     default keyword-only argument specifies an object to return if
+#     the provided iterable is empty.
+#     With two or more arguments, return the largest argument.
+
+# ALTERNATIVE TO help() is ?function - which doesn't work in VS code but does work in data camp's Python editor
+
+
+# Exercises
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1
+print(type(var1)) # class 'list'
+
+# Print out length of var1
+print(len(var1)) # 4
+
+# Convert var2 to an integer: out2
+out2 = int(var2) 
---
+++ Data Camp-intro to Python/lists.py
@@ -0,0 +1,216 @@
+# ******LISTS ******
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list called areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
+
+
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(areas) #['hallway', 11.25, 'kitchen', 18.0, 'living room', 20.0, 'bedroom', 10.75, 'bathroom', 9.5]
+
+
+#More examples of lists
+my_list = [1, 3, 4, 2]
+print(my_list) #[1, 3, 4, 2]
+
+my_list = [[1, 2, 3], [4, 5, 7]]
+print(my_list) #[[1, 2, 3], [4, 5, 7]]
+
+my_list = [1 + 2, "a" * 5, 3]
+print(my_list) #[3, 'aaaaa', 3]
+
+
+# LIST OF LISTS
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed], 
+         ["bathroom", bath]]
+
+# Print out house
+print(house) # [['hallway', 11.25], ['kitchen', 18.0], ['living room', 20.0], ['bedroom', 10.75], ['bathroom', 9.5]]
+
+# Print out the type of house
+print(type(house)) #list
+
+
+# ****SUBSETTING LISTS****
+
+# Use index to access items in list
+# 1st item always has index 0
+# Can count backwards and use negative values to access end items
+# 1st item backwards has index -1
+fam = ["liz", 1.73, "emma", 1.68, "mom", 1.71, "dad", 1.89]
+fam[6] #dad
+fam[-2] #dad
+
+# List slicing
+# Lets you select multiple items in list & create a new list
+fam[3:5] # [1.68, 'mom'] only returns the 4th/5th items, does not return 6th item
+
+#[start: end] start = inclusive, end = exclusive
+
+fam[1:4] # [1.73, "emma", 1.68] 2nd, 3rd, 4th items
+
+# can omit the index before or after the colon
+# if no start index, then starts at index=0
+fam[:4] #['liz', 1.73, "emma", 1.68]
+
+#if no end index, will include all elements INCLUDING  the last value
+fam[5:] #[1.71, 'dad', 1.89]
+
+
+#EXERCISES
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1]) #11.25
+
+# Print out last element from areas
+print(areas[-1]) #9.50
+
+# Print out the area of the living room
+print(areas[-5]) #20.0
+
+
+#Subset & calculate
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[-3] #adding the areas of kitchen & bedroom
+
+
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area) #28.75
+
+
+# Slicing and dicing
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Use slicing to create downstairs
+downstairs = areas[0:6]
+
+# Use slicing to create upstairs
+upstairs = areas[-4:] #have no ending index - so starts at 4th item from end and includes all items after to the end
+upstairs_alt = areas[6:10] # includes 6th, 7th, 8th, 9th items but NOT the 10th
+
+# Print out downstairs and upstairs
+print(downstairs) # ['hallway', 11.25, 'kitchen', 18.0, 'living room', 20.0]
+print(upstairs) #['bedroom', 10.75, 'bathroom', 9.5]
+print(upstairs_alt) # ['bedroom', 10.75, 'bathroom', 9.5]
+
+
+#Omitting the beginning or ending indices 
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Alternative slicing to create downstairs
+downstairs = areas[:6]
+
+# Alternative slicing to create upstairs
+upstairs = areas[-3:]
+
+print(downstairs) #['hallway', 11.25, 'kitchen', 18.0, 'living room', 20.0]
+print(upstairs) #[10.75, 'bathroom', 9.5]
+
+
+# REPLACE LIST ELEMENTS
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+areas[-1] = 10.50
+
+# Change "living room" to "chill zone"
+areas[4] = "chill zone"
+
+# Print new areas list
+print(areas) #['hallway', 11.25, 'kitchen', 18.0, 'chill zone', 20.0, 'bedroom', 10.75, 'bathroom', 10.5]
+
+
+
+# EXTEND A LIST
+# Use the + operator to add more elements to a list
+# Create the areas list and make some changes
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50]
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areas + ["poolhouse", 24.5]
+
+print(areas_1) #['hallway', 11.25, 'kitchen', 18.0, 'chill zone', 20.0, 'bedroom', 10.75, 'bathroom', 10.5, 'poolhouse', 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage", 15.45]
+
+print(areas_2) #['hallway', 11.25, 'kitchen', 18.0, 'chill zone', 20.0, 'bedroom', 10.75, 'bathroom', 10.5, 'poolhouse', 24.5, 'garage', 15.45]
+
+
+
+# DELETE LIST ELEMENTS
+# Once you remove an element from a list, the indexes of the elements that come after the deleted element all change! So you remove 1st element, indices change, then you remove 2nd element, and indices change again.
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+# Same line
+#command1; command2
+
+# Separate lines
+#command1
+#command2
+
+#del(areas[10]); del(areas[11]) # removes the 10th/11th indices - poolhouse, 24.5
+
+del(areas[-4:-2])
+print(areas) #['hallway', 11.25, 'kitchen', 18.0, 'chill zone', 20.0, 'bedroom', 10.75, 'bathroom', 10.5, 24.5, 15.45]
+
+
+
+# INNER WORKINGS OF LISTS
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy - use list(array_name) to prevent changes to the original areas array w/o this then printing areas would have the same changes as areas_copy
+areas_copy = list(areas)
+
+# Change areas_copy
+areas_copy[0] = 5.0
+
+
+# Print areas
+print(areas) #[11.25, 18.0, 20.0, 10.75, 9.5]
+print(areas_copy) #[5.0, 18.0, 20.0, 10.75, 9.5]
+
