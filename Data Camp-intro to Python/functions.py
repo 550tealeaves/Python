2323ee7e5d1dc4848b29b51424e8861e50ca39d0
550tealeaves <nastasia.claire89@gmail.com>
2024-06-02 19:36:41 -0400

    completed function exercises

--- Data Camp-intro to Python/functions.py
+++ Data Camp-intro to Python/functions.py
@@ -53,3 +53,26 @@ print(len(var1)) # 4
 
 # Convert var2 to an integer: out2
 out2 = int(var2) 
+
+
+help(sorted) #sorted(iterable, /, *, key=None, reverse=False)
+    # Return a new list containing all items from the iterable in ascending order.        
+    # A custom key function can be supplied to customize the sort order, and the
+    # reverse flag can be set to request the result in descending order.
+    #BY DEFAULT, key=None & reverse=False
+
+
+# SORTED()
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+print(full) # [11.25, 18.0, 20.0, 10.75, 9.5]
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full, reverse=True)
+
+# Print out full_sorted
+print(full_sorted) #[20.0, 18.0, 11.25, 10.75, 9.5]
