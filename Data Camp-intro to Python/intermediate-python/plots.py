f2024f1b41bc8060897138eb2b4a6d84bb87bd72
550tealeaves <nastasia.claire89@gmail.com>
2024-06-16 02:45:11 -0400

    moved things around

---
+++ Data Camp-intro to Python/intermediate-python/plots.py
@@ -0,0 +1,7 @@
+	# • Matplotlib is the main package for visualizations
+	# 	○ Pyplot is one of the subpackages
+	# 		§ Uses 2 lists as arguments
+	# 			□ 1st list = horizontal axis
+	# 			□ 2nd list = vertical axis
+	# 		§ Call plt.show() to display chart
+	# 	○ Plot function tells what to plot 
