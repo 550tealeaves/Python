f3e9c3bd19a96279a3dd9f8b0408c43a10695120
550tealeaves <nastasia.claire89@gmail.com>
2024-06-19 00:40:28 -0400

    finished dictionaries code

--- Data Camp-intro to Python/intermediate-python/dictionaries.py
+++ Data Camp-intro to Python/intermediate-python/dictionaries.py
@@ -61,3 +61,112 @@ print(europe.values())
 
 # Print out value that belongs to key 'norway'
 print(europe['norway'])
+
+
+##DICTIONARIES PART 2
+# 	• Keys should be immutable objects
+# 		○ Stings, floats, booleans are immutable
+# 		○ But lists are mutable = can change the contents
+	
+	
+	
+# Principality of Sealand
+# 	• To add the country of Sealand and it's population to the world dictionary that was already created, 
+# 		○ World['sealand'] = 0.000027
+# 	• If you call world, sealand will be included 
+# 	• Can also check using a boolean 
+
+
+	# • To update a value within the dictionary, 
+	# 	○ Each key in dictionary is unique, so Python knows that you are updating the value instead of replacing the key 
+	# 	○ World['sealand'] = 0.000028
+	# • To remove a key/value pair in the dictionary, use del
+	# 	○ Del(world['sealand'])
+	# 		§ Will remove sealand & corresponding value
+	# 	○ Print(world)
+	# 		§ Sealand is gone
+
+## EXAMPLE
+world['sealand'] = 0.00027
+
+#check to see that new key/value pair added
+world #adds sealand & value to the dictionary
+
+#can also check if key is in dictionary using boolean
+"sealand" in world # TRUE
+
+#Update the value of the key, "sealand"
+world['sealand'] = 0.0045
+world
+
+#Delete a key value pair
+del(world['sealand'])
+world #removes sealand as key/value pair
+
+
+
+
+##EXERCISE 1 - ADDING MORE KEY/VALUE PAIRS
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe['italy'] = 'rome'
+
+# Print out italy in europe
+print('italy' in europe)
+
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Print europe
+print(europe)
+
+
+
+
+
+
+
+
+## EXERCISE 2 - DICTIONARY MANIPULATION
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe['germany'] = 'berlin'
+
+# Remove australia
+del(europe['australia'])
+
+# Print europe
+print(europe)
+
+
+
+
+
+
+
+
+## EXERCISE 3 - DICTIONARICEPTION
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = {'capital':'rome', 'population': 59.83}
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+# Print europe
+print(europe)
