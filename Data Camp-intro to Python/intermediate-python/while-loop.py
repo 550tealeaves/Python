0151eefa70889d3ea120267f14b1044c92aacf01
550tealeaves <nastasia.claire89@gmail.com>
2024-06-23 17:53:25 -0400

    started while loop code

---
+++ Data Camp-intro to Python/intermediate-python/while-loop.py
@@ -0,0 +1,13 @@
+	• If condition is true, python executes the code only 1 time
+	• While loop 
+		○ Executes code inside if condition is true
+		○ Will continue to execute the code repeatedly 
+	• Similar syntax as if statement
+	• When to use it
+		○ Numerically calculating model
+		○ Repeating action until condition is met
+	• If there is a condition that evaluates to False, then the while loop stops running
+
+
+	• When the while loop keeps running, it disconnects the session
+	• Interrupt the loop with Ctrl + C
