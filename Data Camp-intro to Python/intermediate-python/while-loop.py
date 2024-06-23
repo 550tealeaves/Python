5be3b56e60491790ef91b7b0fd33c3ccf84d909b
550tealeaves <nastasia.claire89@gmail.com>
2024-06-23 17:55:49 -0400

    added more notes

--- Data Camp-intro to Python/intermediate-python/while-loop.py
+++ Data Camp-intro to Python/intermediate-python/while-loop.py
@@ -1,13 +1,25 @@
-	• If condition is true, python executes the code only 1 time
-	• While loop 
-		○ Executes code inside if condition is true
-		○ Will continue to execute the code repeatedly 
-	• Similar syntax as if statement
-	• When to use it
-		○ Numerically calculating model
-		○ Repeating action until condition is met
-	• If there is a condition that evaluates to False, then the while loop stops running
+	# • If condition is true, python executes the code only 1 time
+	# • While loop 
+	# 	○ Executes code inside if condition is true
+	# 	○ Will continue to execute the code repeatedly 
+	# • Similar syntax as if statement
+	# • When to use it
+	# 	○ Numerically calculating model
+	# 	○ Repeating action until condition is met
+	# • If there is a condition that evaluates to False, then the while loop stops running
 
 
-	• When the while loop keeps running, it disconnects the session
-	• Interrupt the loop with Ctrl + C
+	# • When the while loop keeps running, it disconnects the session
+	# • Interrupt the loop with Ctrl + C
+
+
+error = 50.0
+while error > 1:
+    error = error / 4
+    print(error) #0.78125
+
+
+#This while loop will keep going and disconnect the session
+error = 50.0
+while error > 1:
+    (error)
