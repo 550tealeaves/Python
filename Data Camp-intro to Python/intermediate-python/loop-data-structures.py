ab06e8dfe0ddb134c938054e92883123d0634731
550tealeaves <nastasia.claire89@gmail.com>
2024-06-25 20:23:14 -0400

    adding code

---
+++ Data Camp-intro to Python/intermediate-python/loop-data-structures.py
@@ -0,0 +1,13 @@
+world = {
+    "afghanistan": 30.55,
+    "albania": 2.77,
+    "algeria": 39.21
+}
+
+for key, value in world.items():
+    print(key + "--" + str(value))
+
+
+import numpy as np 
+np_height = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
+np_weight = np.array([65.4, 59.2, 63.6, 88.4, 68.7])
