1824efc5a47f5527d3652480de71acb1a6cb62cf
550tealeaves <nastasia.claire89@gmail.com>
2024-07-07 16:15:24 -0400

    added intro notes

---
+++ Data Camp-intro to Python/pre-processing-ml/intro.py
@@ -0,0 +1,9 @@
+##EX 1 - DROP MISSING DATA
+# Drop the Latitude and Longitude columns from volunteer
+volunteer_cols = volunteer.drop(["Latitude", "Longitude"], axis=1)
+
+# Drop rows with missing category_desc values from volunteer_cols
+volunteer_subset = volunteer_cols.dropna(subset=["category_desc"])
+
+# Print out the shape of the subset
+print(volunteer_subset.shape) #617, 33
