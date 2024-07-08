dc1f17633a70a88eba57ed934044fdef2b8f8126
550tealeaves <nastasia.claire89@gmail.com>
2024-07-08 02:39:32 -0400

    added notes

--- Data Camp-intro to Python/pre-processing-ml/intro.py
+++ Data Camp-intro to Python/pre-processing-ml/intro.py
@@ -88,3 +88,26 @@ print(volunteer.dtypes)
 # NTA                   float64
 # dtype: object
 
+
+
+
+
+# Create a DataFrame with all columns except category_desc
+X = volunteer.drop("category_desc", axis=1)
+
+# Create a category_desc labels dataset
+y = volunteer[["category_desc"]]
+
+# Use stratified sampling to split up the dataset according to the y dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+
+# Print the category_desc counts from y_train
+print(y_train["category_desc"].value_counts())
+
+# Strengthening Communities    230
+# Helping Neighbors in Need     89
+# Education                     69
+# Health                        39
+# Environment                   24
+# Emergency Preparedness        11
+# Name: category_desc, dtype: int64
