f0e6159622e3d65fe76852cb99a4b74bd9e0957c
550tealeaves <nastasia.claire89@gmail.com>
2024-07-09 23:57:59 -0400

    added k nearest neighbord code

---
+++ Data Camp-intro to Python/supervised-learning/k-nearest-neighbor.py
@@ -0,0 +1,122 @@
+# k-Nearest Neighbors: Fit
+# In this exercise, you will build your first classification model using the churn_df dataset, which has been preloaded for the remainder of the chapter.
+
+# The target, "churn", needs to be a single column with the same number of observations as the feature data. The feature data has already been converted into numpy arrays.
+
+
+# Import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier 
+
+y = churn_df["churn"].values
+X = churn_df[["account_length", "customer_service_calls"]].values
+
+# Create a KNN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+
+
+# Now you have fit a KNN classifier, you can use it to predict the label of new data points. All available data was used for training, however, fortunately, there are new observations available. These have been preloaded for you as X_new.
+
+# The model knn, which you created and fit the data in the last exercise, has been preloaded for you. You will use your classifier to predict the labels of a set of new data points:
+
+# Predict the labels for the X_new
+y_pred = knn.predict(X_new)
+
+# Print the predictions
+print("Predictions: {}".format(y_pred)) # Predictions: [0 1 0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Import the module
+from sklearn.model_selection import train_test_split
+
+X = churn_df.drop("churn", axis=1).values
+y = churn_df["churn"].values
+
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+
+
+
+
+
+
+
+
+# It's time to practice splitting your data into training and test sets with the churn_df dataset!
+
+# NumPy arrays have been created for you containing the features as X and the target variable as y.
+# Import the module
+from sklearn.model_selection import train_test_split
+
+X = churn_df.drop("churn", axis=1).values
+y = churn_df["churn"].values
+
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))  #0.8740629685157422
+
+
+
+
+
+
+
+
+
+
+
+# Interpreting model complexity is a great way to evaluate supervised learning performance. Your aim is to produce a model that can interpret the relationship between features and the target variable, as well as generalize well when exposed to new observations.
+
+
+#overfitting/underfitting
+
+# Create neighbors
+
+
+
+
+
+##DRAWING A VISUALIZATION
+# Add a title
+plt.title("KNN: Varying Number of Neighbors")
+
+# Plot training accuracies
+plt.plot(neighbors, train_accuracies.values(), label="Training Accuracy")
+
+# Plot test accuracies
+plt.plot(neighbors, test_accuracies.values(), label="Testing Accuracy")
+
+plt.legend()
+plt.xlabel("Number of Neighbors")
+plt.ylabel("Accuracy")
+
+# Display the plot
+plt.show()
+
